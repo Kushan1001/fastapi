d35b0801d707291e30b5b72bede254b692af1c2d
Kushan1001 <kushan1001@gmail.com>
2024-10-07 15:18:30 +0530

    added code

---
+++ main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from aksharamukha import transliterate
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],  
+)
+
+class TextRequest(BaseModel):
+    text: str
+
+def transliterate_text(text : str) -> str:
+    return transliterate.process("autodetect", "ISO", text)
+
+@app.post("/transliterate")
+async def transiterated_text(input_text: TextRequest):
+    text = input_text.text
+    result = transliterate_text(text)
+    return {'transiterated_text':result}
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host='127.0.0.1', port=8080)
+
+
