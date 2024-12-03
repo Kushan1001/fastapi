32c14024bcdbcdb8c80864bbb97f82a98f17b3a3
Kushan1001 <kushan1001@gmail.com>
2024-12-03 17:03:38 +0530

    Added Urdu functionality

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from aksharamukha import transliterate
+from deep_translator import GoogleTranslator
 
 app = FastAPI()
 
@@ -16,13 +16,14 @@ app.add_middleware(
 class TextRequest(BaseModel):
     text: str
 
-def transliterate_text(text : str) -> str:
-    return transliterate.process("autodetect", "ISO", text)
+@app.get('/')
+def homepage():
+    return {"message": "success"}
 
 @app.post("/transliterate")
 async def transiterated_text(input_text: TextRequest):
     text = input_text.text
-    result = transliterate_text(text)
+    result = GoogleTranslator(source='auto', target='en').translate(text)
     return {'transiterated_text':result}
 
 
